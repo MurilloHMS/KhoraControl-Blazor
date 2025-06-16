df7dba3d0e65890b8b6dd80c3f84e1252386fb21
MurilloHMS <murillohenriquemelo@outlook.com>
2025-06-16 11:22:26 -0300

    Initial commit

---
+++ KhoraControl/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KhoraControl
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KhoraControl/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KhoraControl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
