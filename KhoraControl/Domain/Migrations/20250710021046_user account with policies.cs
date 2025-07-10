23c0b9f846c49c4dce7d3401633715cf34b7a264
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-10 20:13:11 -0300

    fix: fixed migration

--- KhoraControl/Domain/Migrations/20250710021046_user account with policies.cs
+++ KhoraControl/Domain/Migrations/20250710021046_user account with policies.cs
@@ -13,56 +13,6 @@ namespace KhoraControl.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Fornecedores",
-                columns: table => new
-                {
-                    ID = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    CNPJ = table.Column<string>(type: "TEXT", nullable: true),
-                    RazaoSocial = table.Column<string>(type: "TEXT", nullable: true),
-                    NomeFantasia = table.Column<string>(type: "TEXT", nullable: true),
-                    InscricaoEstadual = table.Column<string>(type: "TEXT", nullable: true),
-                    CodRegimeTributario = table.Column<string>(type: "TEXT", nullable: true),
-                    Logradouro = table.Column<string>(type: "TEXT", nullable: true),
-                    Numero = table.Column<string>(type: "TEXT", nullable: true),
-                    Complemento = table.Column<string>(type: "TEXT", nullable: true),
-                    Bairro = table.Column<string>(type: "TEXT", nullable: true),
-                    Municipio = table.Column<string>(type: "TEXT", nullable: true),
-                    UF = table.Column<string>(type: "TEXT", nullable: true),
-                    CEP = table.Column<string>(type: "TEXT", nullable: true),
-                    CodigoPais = table.Column<string>(type: "TEXT", nullable: true),
-                    Pais = table.Column<string>(type: "TEXT", nullable: true),
-                    Telefone = table.Column<string>(type: "TEXT", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Fornecedores", x => x.ID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Produtos",
-                columns: table => new
-                {
-                    PRONID_PRO = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    PRODDATCAD = table.Column<DateTime>(type: "TEXT", nullable: false),
-                    PROCCODINT = table.Column<string>(type: "TEXT", nullable: true),
-                    PROCDESCR = table.Column<string>(type: "TEXT", nullable: false),
-                    PROCREF = table.Column<string>(type: "TEXT", nullable: true),
-                    PROCFOR = table.Column<string>(type: "TEXT", nullable: false),
-                    PRONQUANT = table.Column<int>(type: "INTEGER", nullable: false),
-                    PRODDATVAL = table.Column<DateTime>(type: "TEXT", nullable: true),
-                    PROCSTAT = table.Column<string>(type: "TEXT", nullable: true),
-                    PROCNNUMNF = table.Column<string>(type: "TEXT", nullable: true),
-                    PRODDATALT = table.Column<DateTime>(type: "TEXT", nullable: true),
-                    PROCUSRCAD = table.Column<string>(type: "TEXT", nullable: true),
-                    PROCUSRALT = table.Column<string>(type: "TEXT", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Produtos", x => x.PRONID_PRO);
-                });
 
             migrationBuilder.CreateTable(
                 name: "user_account",
@@ -113,11 +63,6 @@ namespace KhoraControl.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Fornecedores");
-
-            migrationBuilder.DropTable(
-                name: "Produtos");
 
             migrationBuilder.DropTable(
                 name: "user_account");
